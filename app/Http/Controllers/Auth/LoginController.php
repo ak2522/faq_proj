d98ddf6b0e8907260dbc25a2e5fa24c8664eb285
anupama <ak2522@njit.edu>
2018-12-09 18:52:25 -0500

    Feature: changes made in methods for Google Authentication(HandleproviderCallback and redirectToProvider)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -34,6 +37,51 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+       $this->middleware('guest')->except('logout');
     }
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->redirect();
+
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+        //dd($user);
+        //return $user->name;
+        //return $user->avatar;
+
+       $find_user = User::where('email', $user->getEmail())->first();
+
+       if ($find_user) {
+           Auth::login($find_user);}
+       else {
+//add user to database
+           $new_user = new User;
+           $new_user->email = $user->getEmail();
+           $new_user->provider_id = $user->getId();
+           $new_user->name = $user->getName();
+           $new_user->avatar = $user->getAvatar();
+           $new_user->save();
+           Auth::login($new_user);
+       }
+
+        //login the user
+        //return redirect($this->redirectTo);
+        return redirect('home');
+
+    }
+
+
 }
