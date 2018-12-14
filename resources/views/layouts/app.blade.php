f7c98d0d65e4d65cbf73da909f44e3c161fcd4aa
anupama <ak2522@njit.edu>
2018-12-14 14:29:31 -0500

    Feature: Changes made to verify recaptcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,17 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
 
-    
+    <script>
+
+        $(function() {
+            $('#form').submit(function(event) {
+                var verified = grecaptcha.getResponse();
+                if (verified.length === 0) {
+                    event.preventDefault();
+                }
+            });
+        });
+    </script>
 
 
         <main class="py-4">
